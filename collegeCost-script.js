a5bdae16b841026ad5cecc2c8065925b210a5209
kpien <kpien@smith.edu>
2018-04-24 17:42:28 -0400

    project costs, remove old school yrs, add future yrs

--- collegeCost-script.js
+++ collegeCost-script.js
@@ -7,12 +7,12 @@ searchBtn.onclick = selectCollege;
 let selectedColleges = [];
 let selectedYears = [];
 let filteredData;
-// let lastSelectedColleges = selectedColleges;
-// let lastSelectedYears = ;
+// let lastSelectedColleges;
+// let lastSelectedYears;
 
 function selectCollege(e) {
-e.preventDefault();
-findSelections();	
+	e.preventDefault();
+	findSelections();
 }
 
 function findSelections() {
@@ -21,20 +21,20 @@ function findSelections() {
 	selectedYears = [];
 
 	//find checked colleges
-d3.selectAll('.collegeBox').each(function(d) {
+	d3.selectAll('.collegeBox').each(function(d) {
 	cb = d3.select(this);
 	if(cb.property("checked")) {
 		selectedColleges.push(cb.property("value"));
 	}
-});
+	});
 
-//find years of attendance from selected start yr
-startYr = parseInt($('#startYr').val().split("-")[0]);
-for(let s=1; s < 5; s++) {
-	let year = startYr.toString() + "-" + (startYr+1).toString();
-	selectedYears.push(year);
-	startYr++;
-}
+	//find years of attendance from selected start yr
+	startYr = parseInt($('#startYr').val().split("-")[0]);
+	for(let s=1; s < 5; s++) {
+		let year = startYr.toString() + "-" + (startYr+1).toString();
+		selectedYears.push(year);
+		startYr++;
+	}
 
 // if (lastSelectedColleges == selectedColleges && lastSelectedYears == selectedYears) {
 	//console.log("in if");
@@ -52,61 +52,80 @@ for(let s=1; s < 5; s++) {
 //}
 }
 
-function projectAmherstCost(year) { 
-	let projectedCost = (year-1988)/0.0004273;
-	return projectedCost;
-}
+function projectCost(school, year) {
+	if (school == "Amherst College") {
+		return projectAmherstCost(year);
+	}
+	else if (school == "Hampshire College") {
+		return projectHampCost(year);
+	}
 
-function projectHampshireCost(year) {
-	let projectedCost = (Year-1985)/0.0004805;
-	return projectedCost;
-}
+	else if (school == "Mount Holyoke College") {
+		return projectMoHoCost(year);
+	}
 
-function projectMoHoCost(year) {
-	let projectedCost = (year-1961)/0.0009341;
-	return projectedCost;
-}
+	else if (school == "Smith College") {
+		return projectSmithCost(year);
+	}
 
-function projectSmithCost(year) {
-	let projectedCost = (year-1983)/0.00052;
-	return projectedCost;
-}
+	else if (school == "University of Massachusetts-Amherst") {
+		return projectUMassCost(year);
+	}
 
-function projectUMassCost(year) { 
-	let projectedCost = (year - 1988)/0.001009;
-	return projectedCost;
+	function projectAmherstCost(year) { 
+		let projectedCost = (year-1988)/0.0004273;
+		return projectedCost;
+	}
+
+	function projectHampCost(year) {
+		let projectedCost = (year-1985)/0.0004805;
+		return projectedCost;
+	}
+
+	function projectMoHoCost(year) {
+		let projectedCost = (year-1961)/0.0009341;
+		return projectedCost;
+	}
+
+	function projectSmithCost(year) {
+		let projectedCost = (year-1983)/0.00052;
+		return projectedCost;
+	}
+
+	function projectUMassCost(year) { 
+		let projectedCost = (year - 1988)/0.001009;
+		return projectedCost;
+	}
 }
 
 function drawChart() {
-//remove prior graph
-d3.selectAll("svg > *").remove();
+	//remove prior graph
+	d3.selectAll("svg > *").remove();
 
-//let collegeLinks = {"Amherst College": "amherst.edu", "Hampshire College": , "Mount Holyoke College": "mtholyoke.edu", "Smith College": "smith.edu", "University of Massachusetts-Amherst", "umass.edu"};
+	//stacked bar chart adapted from https://bl.ocks.org/mbostock/3886208
+	let svg = d3.select("svg"),
+	margin = {top: 20, right: 20, bottom: 30, left: 40},
+	width = +svg.attr("width") - margin.left - margin.right,
+	height = +svg.attr("height") - margin.top - margin.bottom,
+	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//stacked bar chart adapted from https://bl.ocks.org/mbostock/3886208
-let svg = d3.select("svg"),
-margin = {top: 20, right: 20, bottom: 30, left: 40},
-width = +svg.attr("width") - margin.left - margin.right,
-height = +svg.attr("height") - margin.top - margin.bottom,
-g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	let x = d3.scaleBand()
+	    .rangeRound([0, width-80])
+	    .paddingInner(0.05)
+	    .align(0.1);
 
-let x = d3.scaleBand()
-    .rangeRound([0, width-80])
-    .paddingInner(0.05)
-    .align(0.1);
+	let y = d3.scaleLinear()
+	    .rangeRound([height, 0]);
 
-let y = d3.scaleLinear()
-    .rangeRound([height, 0]);
+	let z = d3.scaleOrdinal()
+	    .range(["#191970", "#0018A8", "#1F75FE", "#89CFF0"]);
 
-let z = d3.scaleOrdinal()
-    .range(["#191970", "#0018A8", "#1F75FE", "#89CFF0"]);
-
-d3.csv("collegeData.csv", function(d, i, columns) {
-  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-  d.total = t;
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+	d3.csv("collegeData.csv", function(d, i, columns) {
+	  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+	  d.total = t;
+	  return d;
+	}, function(error, data) {
+	  if (error) throw error;
 
 	//multiple checkbox filtering adapted from https://bl.ocks.org/johnnygizmo/531991a77047112b7ca89f78b840fba5
 	//filter data to include only selected colleges
@@ -121,14 +140,23 @@ d3.csv("collegeData.csv", function(d, i, columns) {
 	//filter data to include only selected years
 	let yrFilteredData = [];
 	for(let j = 0; j < filteredData.length; j++) {
-		let yrFilteredOneCollegeData = {"INSTNM": filteredData[j]["INSTNM"]};
+		let schoolData = filteredData[j];
+		let yrFilteredOneCollegeData = {"INSTNM": schoolData["INSTNM"]};
 		let selCosts = [];
-		Object.keys(filteredData[j]).forEach(function(key) {
-			if (selectedYears.includes(key)) {
-				yrFilteredOneCollegeData[key] = filteredData[j][key];
-				selCosts.push(filteredData[j][key]);
+		for(let k = 0; k < selectedYears.length; k++) {
+			let schoolYr = selectedYears[k];
+			if(schoolYr in schoolData) {
+				let yearCost = schoolData[schoolYr];
+				yrFilteredOneCollegeData[schoolYr] = yearCost;
+				selCosts.push(yearCost);
 			}
-		})
+			else {
+				let year = parseInt(schoolYr.split("-")[0]);
+				let yearProjectedCost = projectCost(filteredData[j]["INSTNM"], year);
+				yrFilteredOneCollegeData[schoolYr] = yearProjectedCost;
+				selCosts.push(yearProjectedCost);
+			}
+		}
 		//add total
 		let sum = selCosts.reduce(function(a, b) {return a + b;},0);
 		yrFilteredOneCollegeData["total"] = sum;
